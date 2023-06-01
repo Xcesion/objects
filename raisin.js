7ee161c9bd34b05948ce8586d58f4c9884cc3222
Frank Shen <frank851207@gmail.com>
2023-06-01 00:44:48 -0700

    countLetter function init

--- raisin.js
+++ raisin.js
@@ -39,3 +39,45 @@ const raisinAlarmArray = function (cookies) {
       ["🍫", "🍫", "🍫"],
     ]));
 
+
+
+const myObject = {
+  a: 6,     // Number
+  b: "abc", // String
+  c: true,  // Boolean
+  d: null,  // Null
+};
+
+
+// const person = {
+//   name: "Paul",
+//   address: {
+//     street: "310 W 95th",
+//     city: "New York",
+//     zipCode: 10027
+//   }
+// };
+
+// person["phoneNumbers"] = 6045881234
+// console.log(person["phoneNumbers"] instanceof String);
+// const spam = "spam";
+// person["dislikes"] = { food: spam, "e-mail": "spam" }
+
+// console.log(person)
+
+
+// var planetMoons = {
+//   mercury: 0,
+//   venus: 0,
+//   earth: 1,
+//   mars: 2,
+//   jupiter: 67,
+//   saturn: 62,
+//   uranus: 27,
+//   neptune: 14
+// };
+
+// for (var planet in planetMoons) {
+//   var numberOfMoons = planetMoons[planet];
+//   console.log("Planet: " + planet + ", # of Moons: "+ numberOfMoons);
+// }
