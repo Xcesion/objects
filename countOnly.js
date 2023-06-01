33cd944acbc76cf9e0af02377ad91ee7bdccae0c
Frank Shen <frank851207@gmail.com>
2023-06-01 00:31:21 -0700

    countOnly Function init

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected){
+    console.log(`✅✅✅ Assertion Passed: [ ${actual} ] === [ ${expected} ]`)
+  }else {
+    console.log(`🛑🛑🛑 Assertion Failed: [ ${actual} ] !== [ ${expected} ]`)
+  };
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {
+    if (itemsToCount[item]){
+      if (results[item]) {
+        results[item] += 1;// when ever 
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+console.log(result1);
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
